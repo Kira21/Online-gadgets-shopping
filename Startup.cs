f2be0acb23664ba1259d42dd16a08e7e95eb5f9e
Kira21 <71866987+Kira21@users.noreply.github.com>
2021-06-27 17:51:46 +0530

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AcKyGadgets.Startup))]
+namespace AcKyGadgets
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ StoreController.cs
@@ -0,0 +1,38 @@
+﻿using AcKyGadgets.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AcKyGadgets.Controllers
+{
+    public class StoreController : Controller
+    {
+        // GET: Store
+        AckyGadgetsEntities storeDB = new AckyGadgetsEntities();
+        public ActionResult Index()
+        {
+            var category = storeDB.Categories.ToList();
+
+            return View(category);
+        }
+        [ChildActionOnly]
+        public ActionResult CategoryMenu()
+        {
+            var categories = storeDB.Categories.ToList();
+            return PartialView("categories");
+        }
+        public ActionResult Browse(string category)
+        {
+            var categorymodel = storeDB.Categories.Include("Items").Single(c => c.CategoryName == category);
+            return View(categorymodel);
+        }
+        public ActionResult Details(int id)
+        {
+            var Item = storeDB.Items.Find(id);
+
+            return View(Item);
+        }
+    }
+}
